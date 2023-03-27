98cf97d50bdbc1ed21a830bc2ef4c13ecf24f78d
Kaspar Johannes Pärtel <kasparjohannes@gmail.com>
2023-03-27 15:14:13 +0300

    Spring initializer

---
+++ src/main/java/com/github/kasparpartel/betcalculator/BetCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.github.kasparpartel.betcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/kasparpartel/betcalculator/BetCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kasparpartel.betcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
